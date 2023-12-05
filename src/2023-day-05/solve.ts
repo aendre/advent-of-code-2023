b28ba6a4bf3ed0c0b196149335241395c6ff1f00
eandras <eandras@chemaxon.com>
2023-12-05 13:18:54 +0100

    Day 5, 2023

--- src/2023-day-05/solve.ts
+++ src/2023-day-05/solve.ts
@@ -1,10 +1,6 @@
+import { count } from 'console';
 import { _, math, patternMatch } from '../utils/libs.js';
 import { AocPuzzle } from '../utils/aoc.js';
-
-// Array:  _.first, _.last, _.chunk, _.difference, _.intersection, _.union, _.uniq
-// String: trim, endsWith, startsWith, _.words
-// Math: _.max, _.min, _.mean, _.sum
-// Utils: _.times, _.matches, _.over
 
 function seedToMapLocation(seed: number, map:number[][]) {
   let location = seed;
@@ -18,7 +14,7 @@ function seedToMapLocation(seed: number, map:number[][]) {
   return location
 }
 
-let seedToFinalDestination = function seedToFinalDestination(seed:number, maps:number[][][]) {
+function seedToFinalDestination(seed:number, maps:number[][][]) {
   let location = seed
   maps.forEach(map => {
     location = seedToMapLocation(location, map)
@@ -26,46 +22,56 @@ let seedToFinalDestination = function seedToFinalDestination(seed:number, maps:n
   return location
 }
 
-seedToFinalDestination = _.memoize(seedToFinalDestination)
-
 export default function solve(aoc: AocPuzzle) {
   const input = aoc.input().split('\n\n')
   const seeds = input.shift()?.split(': ').pop()?.split(' ')
     .map(AocPuzzle.parseInt) as number[];
   const maps = input.map(m => m.split(':\n').pop()?.split('\n').map(w => _.words(w).map(AocPuzzle.parseInt))) as number[][][];
 
-  const seedGroups = _.chunk(seeds, 2).map(ranges => {
-    const [start, count] = ranges;
-    return { start, count }
+  // Part 1 begins
+  const part1 = seeds.map(seed => seedToFinalDestination(seed, maps))
+  AocPuzzle.answer(_.min(part1))
+
+  // Part 2 begins
+  const seedRanges = _.chunk(seeds, 2).map(range => ({ start: range[0], end: range[0] + range[1] }))
+
+  const splitRanges = seedRanges.map(range => {
+    const rangeLength = range.end - range.start
+    const chunkSize = Math.ceil(Math.cbrt(rangeLength))
+    const nrOfChunks = Math.floor(rangeLength / chunkSize)
+
+    return [
+      range.start,
+      ..._.times(nrOfChunks).map(i => range.start + (i + 1) * chunkSize),
+      range.end,
+    ]
   })
 
-  let min = seedToFinalDestination(seedGroups[0].start, maps)
-  seedGroups.forEach(group => {
-    for (let i = group.start; i <= group.start + group.count; i += 1) {
-      let current = seedToFinalDestination(i, maps)
-      if (current < min) {
-        min = current
-      }
+  const part2partial = splitRanges.map(seedRange => seedRange.map((seed, index) => ({
+    seed,
+    location: seedToFinalDestination(seed, maps),
+    index,
+  })))
+    .map(seedRange => _.minBy(seedRange, 'location')!)
+
+  const part2 = part2partial.map((bestSeed, groupIndex) => {
+    let start; let end;
+    if (bestSeed?.index === 0) {
+      start = splitRanges[groupIndex][bestSeed.index]
+      end = splitRanges[groupIndex][bestSeed.index + 1]
+    } else if (bestSeed.index === splitRanges[groupIndex].length - 1) {
+      start = splitRanges[groupIndex][bestSeed.index - 1]
+      end = splitRanges[groupIndex][bestSeed.index]
+    } else {
+      start = splitRanges[groupIndex][bestSeed.index - 1]
+      end = splitRanges[groupIndex][bestSeed.index + 1]
     }
+    return _.range(start, end + 1)
   })
 
-  AocPuzzle.answer(min)
+  const part2final = part2.flat().map(seed => seedToFinalDestination(seed, maps))
+  const part2finalfinal = _.min(part2final) // remebering those good old Photoshop layer names :)
 
-  // const lowest = _.minBy(moreSeeds, 'count')!
-  // console.log(lowest)
-  // const moreSeeds2 = _.range(79, 79 + 14s)
-  // console.log(moreSeeds2)
-
-  // let location: number[] = [...moreSeeds2]
-  // maps.forEach(map => {
-  //   location = seedToLocation(location, map)
-  // })
-
-  // const part1 = seeds.map(seed => seedToFinalDestination(seed, maps))
-  // AocPuzzle.answer(_.min(part1))
-
-  // console.log(seeds.every((seed,i)=> seed<), location)
-  // 3_640_772_818 104_094_365
-
-  // console.log(flatSeeds)
+  // ugly as hell, but I had no more time...
+  AocPuzzle.answer(part2finalfinal)
 }
