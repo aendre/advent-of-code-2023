dbc1cfda0e13e165b12b07b1a023d4721f12d45a
eandras <eandras@chemaxon.com>
2023-12-07 07:51:02 +0100

    Day 7, 2023

---
+++ src/2023-day-07/solve.ts
@@ -0,0 +1,55 @@
+import { _, math, patternMatch } from '../utils/libs.js';
+import { AocPuzzle } from '../utils/aoc.js';
+
+function cleverKinds(hand:string) {
+  const lengths = Array.from(hand).map(letter => hand.split(letter).length - 1)
+  return AocPuzzle.parseInt(lengths.sort((a, b) => b - a).join(''))
+}
+
+function jokerKinds(hand:string) {
+  const lengths = Array.from(hand).filter(c => c !== '0').map(letter => ({
+    letter,
+    occ: hand.split(letter).length - 1,
+  }))
+  lengths.sort((a, b) => b.occ - a.occ)
+  const char = _.maxBy(lengths, 'occ')
+
+  if (typeof char === 'undefined') {
+    return cleverKinds(hand)
+  }
+  return cleverKinds(hand.replaceAll('0', char.letter))
+}
+
+export default function solve(aoc: AocPuzzle) {
+  const input = aoc.input()
+    .lines()
+    .map(line => {
+      let [hand, bid] = line.split(' ')
+      // Z, Y, X, V, T
+      // A, K, Q, J, T
+      hand = hand.replaceAll('A', 'Z')
+        .replaceAll('K', 'Y')
+        .replaceAll('Q', 'X')
+        .replaceAll('J', '0') // Map J to V for part 1
+
+      return { hand, bid: AocPuzzle.parseInt(bid) }
+    })
+
+  input.sort((a, b) => {
+    const kindA = jokerKinds(a.hand);
+    const kindB = jokerKinds(b.hand);
+
+    if (kindA !== kindB) {
+      return kindB - kindA;
+    }
+
+    return b.hand.localeCompare(a.hand)
+  })
+  input.reverse()
+
+  const result = input.map((item, index) => item.bid * (index + 1))
+  // 250520536 is too high
+  // 250341772 is too low
+  // 250146490 - wrong
+  AocPuzzle.answer(_.sum(result))
+}
