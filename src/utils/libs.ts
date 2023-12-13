20b8b1b0a5eb6e4aff1b9f839b3bb1d30dc20025
eandras <eandras@chemaxon.com>
2023-12-14 00:06:54 +0100

    add memoize helper

--- src/utils/libs.ts
+++ src/utils/libs.ts
@@ -70,3 +70,18 @@ export function patternMatch(str: string, matcher:string) {
   // Return inputs in the matching type
   return matches.map((match, index) => regExpMap[tokens[index]].initializer(match));
 }
+
+export function memoize<Args extends unknown[], Result>(func: (...args: Args) => Result): (...args: Args) => Result {
+  const stored = new Map<string, Result>();
+
+  return (...args) => {
+    const k = JSON.stringify(args);
+    if (stored.has(k)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return stored.get(k)!;
+    }
+    const result = func(...args);
+    stored.set(k, result);
+    return result;
+  };
+}
